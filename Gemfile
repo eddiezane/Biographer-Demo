6cdddbcfdb3282be8264b62b36568bb59f0152a5
Eddie Zaneski <eddiezane@gmail.com>
2013-03-31 12:17:38 -0400

    Demo

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'rest-client'
+gem 'json'
+gem 'digest/md5'
--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
+require 'rest-client'
+require 'json'
+require 'digest/md5'
 
-get '/' do
-  File.read(File.join('public', 'index.html'))
+get '/:email' do
+  # File.read(File.join('public', 'index.html'))
+  @email = Digest::MD5.hexdigest(params[:email])
+  url = "http://biographer.herokuapp.com/api/bio/#{@email}"
+  response = RestClient.get(url)
+  @bio = JSON.parse(response.body)
+  erb :index
 end
+
+__END__
+@@index
+<%= "#{@bio['first_name']} #{@bio['last_name']}" %>
+<br>
+<%= @bio["about_me"] %>
+<br>
+<%= @bio["phone_number"] %>
+<br>
+<%= @bio["occupation"] %>
+<br>
+<%= @bio["school"] %>
+
